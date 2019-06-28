f7fe0acb4ec1abc047ceb14ed9c439346f541630
Barbara Buoželienė <49492868+barbaraBzl@users.noreply.github.com>
2019-06-28 18:04:43 +0300

    Create Menu.js
    create menu

---
+++ courses-app/src/Menu.js
@@ -0,0 +1,23 @@
+const Menu=()=>{
+    return<div id="container">
+<ul class="dropdown">
+	<li><span><a class="active">MENIU</a></span></li>
+  <div class="dropdown-content">
+    <li><a>Kategorijos</a>
+      <ul><li><a>Vienas</a></li>
+    <li><a>Du</a></li>
+    <li><a>Trys</a></li>
+    <li><a>Keturi</a></li>
+    <li><a>Penki</a></li>
+      </ul>
+    </li>
+  <li><a  href="vasara.html">Kalendorius</a></li>
+  <li><a  href="contact.asp">Blog'as</a></li>
+  <li><a  href="about.asp">Asmeninis profilis</a></li>
+  <li><a  href="">DUK</a></li>
+  <li><a  href="">Registracija</a></li>
+  <li><a  href="">Prisijungimas</a></li>
+</div>
+</ul>
+</div>
+}
